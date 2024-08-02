e16b5f7ac052ffe7acb48070fb0ae44043a7d009
Swapanth <swapanthvakapalli@gmail.com>
2024-08-02 22:52:48 +0530

    supa

--- frontend/src/libs/supabase/supabase.js
+++ frontend/src/libs/supabase/supabase.js
@@ -3,6 +3,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://yokzoborixhbburgezxf.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlva3pvYm9yaXhoYmJ1cmdlenhmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NjEzODkxMCwiZXhwIjoyMDExNzE0OTEwfQ.TttPyZaW5GSCQMS0Hc-4tWRIi0Gq2aOaUGmqx03k4h8";
+const supabaseKey = "..TttPyZaW5GSCQMS0Hc-4tWRIi0Gq2aOaUGmqx03k4h8";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
