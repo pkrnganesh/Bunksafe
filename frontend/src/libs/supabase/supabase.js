67505983198f52196e57f45346a859b429f102fc
Swapanth <swapanthvakapalli@gmail.com>
2024-08-06 19:29:55 +0530

    refactored

--- frontend/src/libs/supabase/supabase.js
+++
@@ -1,8 +0,0 @@
-// supabase.js
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://yokzoborixhbburgezxf.supabase.co";
-const supabaseKey = "..TttPyZaW5GSCQMS0Hc-4tWRIi0Gq2aOaUGmqx03k4h8";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
