16e4347dd00b233be61262debb45a1c98a0bc3f9
Swapanth Vakapalli <121005043+Swapanth@users.noreply.github.com>
2024-09-20 10:35:36 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,10 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
+app.use(cors({
+  origin: 'https://bunksafe.vercel.app'
+}));
+
 app.use(express.json());
 
 // Using the routes
